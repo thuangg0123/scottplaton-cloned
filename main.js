5ebc3979ef1c5b12aef9a46e85626a1c6887d7b8
thuangg0123 <thuangg456@gmail.com>
2023-03-08 21:25:30 +0700

    push code to github

---
+++ main.js
@@ -0,0 +1,55 @@
+window.addEventListener("load", function(){
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const nextBtn = document.querySelector(".slider-next");
+    const prevBtn = document.querySelector(".slider-prev");
+    const dotItem = document.querySelectorAll(".slider-dot-item");
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    const slidesLength = sliderItems.length;
+    let positionX = 0;
+    let index = 0;
+
+    nextBtn.addEventListener("click", () => {
+        handleChangeSlide(1);
+    })
+
+    prevBtn.addEventListener("click", () => {
+        handleChangeSlide(-1);
+    });
+
+    [...dotItem].forEach((item) => item.addEventListener("click", function(c){
+        [...dotItem].forEach(d => d.classList.remove("active"));
+        c.target.classList.add("active");
+        const slideIndex = parseInt(c.target.dataset.index);
+        index = slideIndex;
+        positionX = -1 * index * sliderItemWidth;
+        sliderMain.style = `transform: translateX(${positionX}px)`
+    }))
+
+    function handleChangeSlide(direction){
+        if(direction == 1){
+            if(index >= slidesLength - 1){
+                index = slidesLength - 1;
+                return;
+            }
+            positionX = positionX - sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`;
+            index++;
+        }
+        else if(direction == -1){
+            if(index <= 0){
+                index = 0;
+                return;
+            }
+            positionX = positionX + sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`;
+            index--;
+        }
+
+        [...dotItem].forEach(d => d.classList.remove("active"));
+        dotItem[index].classList.add("active");
+    }
+})
+
+
